726bd0d83b68d19e7604d39181f218be8ebbfdd3
lchhun <chhunlisa@gmail.com>
2017-07-06 11:37:15 -0400

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -219,3 +219,21 @@ $('.cd-slider-nav li').on('click', function(event){
 		updateNavigationMarker(selectedPosition+1);
 	}
 });
+
+
+
+
+$(function(){ /* to make sure the script runs after page load */
+
+    $('a.read_more').click(function(event){ /* find all a.read_more elements and bind the following code to them */
+
+        event.preventDefault(); /* prevent the a from changing the url */
+        $(this).parents('.item').find('.more_text').show(); /* show the .more_text span */
+
+    });
+
+});
+
+
+
+    
